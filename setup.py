bb62b855980b7351df147214626f74297b08bdb2
Grogu2301 <nikita.chaturvedi2019@gmail.com>
2023-07-20 15:55:54 +0530

    logging and exception added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="cyberbullying-detection",
+    version="0.0.1",
+    author="Nikita",
+    author_email="nikita.chaturvedi2019@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
